320149d9510a941f8b2ac1f0a02790c6e4bbf9d9
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-08 02:34:39 -0300

    feat: created the server

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import Express from "express";
+import userRoutes from "./api/routes/UserRoutes";
+
+const app = Express();
+app.use(Express.json());
+
+const PORT = 8000;
+
+app.get("/api", (req, res) => {
+  return res.send({ message: "Hello World"! });
+});
+
+app.use(Express.json());
+app.use("/api", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running ${PORT}`);
+});
