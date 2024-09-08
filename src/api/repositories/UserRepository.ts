be8c552a9b0393d77df812d91b8c44f9b99891aa
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-08 02:36:08 -0300

    feat: created userRepository

---
+++ src/api/repositories/UserRepository.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import { IUserRepository } from "../interfaces/IUserRepository";
+
+// Create an instance of PrismaClient
+const prisma = new PrismaClient();
+
+// Define a class that implements the IUserRepository interface
+export class UserRepository implements IUserRepository {
+  // Method to create a new user
+  async createUser(data: {
+    name: string;
+    email: string;
+    password: string;
+  }): Promise<any> {
+    // Use the PrismaClient instance to create a new user record in the database
+    return await prisma.user.create({
+      data, // Pass the user data to the Prisma 'create' method
+    });
+  }
+
+  // Method to get all users
+  async getUsers(): Promise<any[]> {
+    // Use the PrismaClient instance to retrieve all user records from the database
+    return await prisma.user.findMany();
+  }
+}
