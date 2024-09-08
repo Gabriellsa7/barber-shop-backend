9c9abbfc9f8d487526c2a3641575eb3aeea874cc
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-08 02:36:23 -0300

    feat: created the userRoutes

---
+++ src/api/routes/userRoutes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+// Import the UserController, which contains the logic for handling user-related HTTP requests
+import UserController from "../controllers/UserController";
+
+// Create a new instance of the Router object
+const routes = Router();
+
+// Define a route for handling POST requests to "/users"
+// When a POST request is made to this endpoint, the createUser method from UserController will be invoked
+routes.post("/users", UserController.createUser);
+
+// Define a route for handling GET requests to "/users"
+// When a GET request is made to this endpoint, the getUsers method from UserController will be invoked
+routes.get("/users", UserController.getUsers);
+
+export default routes;
