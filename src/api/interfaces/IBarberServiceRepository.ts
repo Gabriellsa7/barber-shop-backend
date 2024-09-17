a2a493271c73d908a337083a48295056440002e3
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-17 14:28:04 -0300

    feat: created interface barber service repository

---
+++ src/api/interfaces/IBarberServiceRepository.ts
@@ -0,0 +1,25 @@
+export interface IBarberService {
+  createService(data: {
+    name: string;
+    description: string;
+    price: number;
+    duration: number;
+    barberShop_id: string;
+  }): Promise<any>;
+
+  getService(id: string): Promise<any>;
+
+  getAllServices(): Promise<any>;
+
+  updateService(
+    id: string,
+    data: {
+      name?: string;
+      description?: string;
+      price?: number;
+      duration?: number;
+    }
+  ): Promise<any>;
+
+  deleteService(id: string): Promise<any>;
+}
