4e0fd37762fa98819cfeccf3cb36b4f8ad9b3c3c
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-18 18:56:21 -0300

    feat: created the barber service repository

---
+++ src/api/repositories/BarberServiceRepository.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+import { IBarberService } from "../interfaces/IBarberServiceRepository";
+
+const prisma = new PrismaClient();
+
+export class BarberServiceRepository implements IBarberService {
+  async createService(data: {
+    name: string;
+    description: string;
+    price: number;
+    duration: number;
+    barberShop_id: string;
+  }): Promise<any> {
+    return await prisma.barberService.create({ data });
+  }
+  async getService(id: string): Promise<any> {
+    return await prisma.barberService.findUnique({ where: { id } });
+  }
+  async getAllServices(): Promise<any> {
+    return await prisma.barberService.findMany();
+  }
+  async updateService(
+    id: string,
+    data: {
+      name?: string;
+      description?: string;
+      price?: number;
+      duration?: number;
+    }
+  ): Promise<any> {
+    return await prisma.barberService.update({ where: { id }, data });
+  }
+  async deleteService(id: string): Promise<any> {
+    return await prisma.barberService.delete({ where: { id } });
+  }
+}
