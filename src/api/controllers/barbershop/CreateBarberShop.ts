31742fef438a36c888a5233dd5ffcfa3d5f0024e
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-05 15:39:18 -0300

    feat: change a variable name

--- src/api/controllers/barbershop/CreateBarberShop.ts
+++ src/api/controllers/barbershop/CreateBarberShop.ts
@@ -21,13 +21,13 @@ export default {
         new BarberShopRepository()
       );
 
-      const barberShope = await createBarberShop.createBarberShop(
+      const barberShop = await createBarberShop.createBarberShop(
         name,
         address,
         description
       );
 
-      return res.status(201).send(barberShope);
+      return res.status(201).send(barberShop);
     } catch (error) {
       return res.status(400).json({
         message: "It was not possible to create the barberShop",
