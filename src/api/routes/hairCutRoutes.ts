b66e380670ec882e1375520abbd75bf2da5b72e3
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-05 15:36:16 -0300

    feat: created hairCut router

--- src/api/routes/hairCutRoutes.ts
+++ src/api/routes/hairCutRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import CreateHairCut from "../controllers/haircut/CreateHairCut";
+import UpdateHairCut from "../controllers/haircut/UpdateHairCut";
+import GetAllHairCut from "../controllers/haircut/GetAllHairCut";
 
 const routes = Router();
 
 routes.post("/haircut", CreateHairCut.createHairCut);
+routes.put("/haircut/:id", UpdateHairCut.updateHairCut);
+routes.get("/haircut", GetAllHairCut.getAllHairCut);
 
 export default routes;
