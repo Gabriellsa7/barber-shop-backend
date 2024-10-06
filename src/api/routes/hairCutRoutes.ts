ee62d7ac4cc1487473366350e2a5ea1f740f6168
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-06 08:57:21 -0300

    feat: added the get and delete router to the hair cut

--- src/api/routes/hairCutRoutes.ts
+++ src/api/routes/hairCutRoutes.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import CreateHairCut from "../controllers/haircut/CreateHairCut";
 import UpdateHairCut from "../controllers/haircut/UpdateHairCut";
 import GetAllHairCut from "../controllers/haircut/GetAllHairCut";
+import GetHairCut from "../controllers/haircut/GetHairCut";
+import DeleteHairCut from "../controllers/haircut/DeleteHairCut";
 
 const routes = Router();
 
 routes.post("/haircut", CreateHairCut.createHairCut);
 routes.put("/haircut/:id", UpdateHairCut.updateHairCut);
 routes.get("/haircut", GetAllHairCut.getAllHairCut);
+routes.get("/haircut/:id", GetHairCut.getHairCut);
+routes.delete("/haircut/:id", DeleteHairCut.deleteHairCut);
 
 export default routes;
