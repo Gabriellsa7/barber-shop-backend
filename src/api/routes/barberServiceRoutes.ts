ef9d7e1d11683ee3bfe1573004feef0865f2306b
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-06 09:17:43 -0300

    feat: added a new routes on barber service routes

--- src/api/routes/barberServiceRoutes.ts
+++ src/api/routes/barberServiceRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import CreateBarberService from "../controllers/barberservice/CreateBarberService";
 import GetAllBarberService from "../controllers/barberservice/GetAllBarberService";
+import GetBarberService from "../controllers/barberservice/GetBarberService";
+import UpdateBarberService from "../controllers/barberservice/UpdateBarberService";
+import DeleteBarberService from "../controllers/barberservice/DeleteBarberService";
 
 const routes = Router();
 
 routes.post("/barberservice", CreateBarberService.createBarberService);
 routes.get("/barberservice", GetAllBarberService.getAllBarberService);
+routes.get("/barberservice/:id", GetBarberService.getBarberService);
+routes.put("/barberservice/:id", UpdateBarberService.updateBarberService);
+routes.delete("/barberservice/:id", DeleteBarberService.deleteBarberService);
 
 export default routes;
