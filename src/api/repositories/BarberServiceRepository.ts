f6137c6b16ecdf8f794dec062813b22c40331a8b
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-08 00:32:13 -0300

    feat: changed the promise type

--- src/api/repositories/BarberServiceRepository.ts
+++ src/api/repositories/BarberServiceRepository.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { BarberService, BarberShop, PrismaClient } from "@prisma/client";
 import { IBarberService } from "../interfaces/IBarberServiceRepository";
 
 const prisma = new PrismaClient();
@@ -10,15 +10,15 @@ export class BarberServiceRepository implements IBarberService {
     price: number;
     duration: number;
     barberShop_id: string;
-  }): Promise<any> {
+  }): Promise<BarberService> {
     return await prisma.barberService.create({ data });
   }
 
-  async getService(id: string): Promise<any> {
+  async getService(id: string): Promise<BarberService | null> {
     return await prisma.barberService.findUnique({ where: { id } });
   }
 
-  async getAllServices(): Promise<any> {
+  async getAllServices(): Promise<BarberService[]> {
     return await prisma.barberService.findMany();
   }
 
@@ -31,16 +31,16 @@ export class BarberServiceRepository implements IBarberService {
       duration?: number;
       barberShop_id: string;
     }
-  ): Promise<any> {
+  ): Promise<BarberService> {
     return await prisma.barberService.update({ where: { id }, data });
   }
 
-  async deleteService(id: string): Promise<any> {
+  async deleteService(id: string): Promise<BarberService> {
     return await prisma.barberService.delete({ where: { id } });
   }
 
   // Implementation of getBarberShop
-  async getBarberShop(barberShop_id: string): Promise<any | null> {
+  async getBarberShop(barberShop_id: string): Promise<BarberShop | null> {
     return await prisma.barberShop.findUnique({
       where: { id: barberShop_id },
     });
