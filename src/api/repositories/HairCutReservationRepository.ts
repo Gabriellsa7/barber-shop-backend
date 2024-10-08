4ff7da62efb5b7ebdaca9c34d8cf9eff9eef5fd7
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-08 00:32:01 -0300

    feat: changed the promise type

--- src/api/repositories/HairCutReservationRepository.ts
+++ src/api/repositories/HairCutReservationRepository.ts
@@ -1,4 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import {
+  BarberService,
+  BarberShop,
+  HairCutReservation,
+  PrismaClient,
+  User,
+} from "@prisma/client";
 import { IHairCutReservationRepository } from "../interfaces/IHairCutReservationRepository";
 import { ReservationStatus } from "../enums/ReservationStatus";
 
@@ -14,7 +20,7 @@ export class HairCutReservationRepository
     status: ReservationStatus;
     user_id: string;
     img_url?: string;
-  }): Promise<any> {
+  }): Promise<HairCutReservation> {
     return await prisma.hairCutReservation.create({ data });
   }
 
@@ -28,36 +34,39 @@ export class HairCutReservationRepository
       user_id?: string;
       img_url?: string;
     }
-  ): Promise<any> {
+  ): Promise<HairCutReservation> {
     return await prisma.hairCutReservation.update({ where: { id }, data });
   }
 
-  async getAllReservation(): Promise<any> {
+  async getAllReservation(): Promise<HairCutReservation[]> {
     return await prisma.hairCutReservation.findMany();
   }
 
-  async getReservation(id: string): Promise<any> {
+  async getReservation(id: string): Promise<HairCutReservation | null> {
     return await prisma.hairCutReservation.findUnique({ where: { id } });
   }
 
-  async deleteReservation(id: string): Promise<any> {
+  async deleteReservation(id: string): Promise<HairCutReservation> {
     return await prisma.hairCutReservation.delete({ where: { id } });
   }
 
-  async getBarberServiceId(service_id: string): Promise<any | null> {
-    return await prisma.hairCutReservation.findUnique({
+  // Refatorado para buscar os serviços diretamente da tabela de serviços
+  async getBarberServiceId(service_id: string): Promise<BarberService | null> {
+    return await prisma.barberService.findUnique({
       where: { id: service_id },
     });
   }
 
-  async getBarberShopId(barberShop_id: string): Promise<any | null> {
-    return await prisma.hairCutReservation.findUnique({
+  // Refatorado para buscar a barbearia diretamente da tabela de barbearias
+  async getBarberShopId(barberShop_id: string): Promise<BarberShop | null> {
+    return await prisma.barberShop.findUnique({
       where: { id: barberShop_id },
     });
   }
 
-  async getUserId(user_id: string): Promise<any | null> {
-    return await prisma.hairCutReservation.findUnique({
+  // Refatorado para buscar o usuário diretamente da tabela de usuários
+  async getUserId(user_id: string): Promise<User | null> {
+    return await prisma.user.findUnique({
       where: { id: user_id },
     });
   }
