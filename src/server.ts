e84e4bcd0b5a52ba05f0750a411524ea27405efa
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-09 00:47:46 -0300

    feat: added two more routes in server

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import userRoutes from "./api/routes/userRoutes";
 import barberShopRoutes from "./api/routes/barberShopRoutes";
 import hairCutRoutes from "./api/routes/hairCutRoutes";
 import barberServiceRoutes from "./api/routes/barberServiceRoutes";
-
+import hairCutReservationRoutes from "./api/routes/hairCutReservationRoutes";
+import paymentRoutes from "./api/routes/paymentRoutes";
 const app = Express();
 app.use(Express.json());
 
@@ -18,6 +19,8 @@ app.use("/api", userRoutes);
 app.use("/api", barberShopRoutes);
 app.use("/api", hairCutRoutes);
 app.use("/api", barberServiceRoutes);
+app.use("/api", hairCutReservationRoutes);
+app.use("/api", paymentRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running ${PORT}`);
