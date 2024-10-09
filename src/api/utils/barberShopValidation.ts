2ec169ccd005e5c36312c36ed8d3ff516b9eb909
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-10-09 00:48:40 -0300

    feat: added more attributes to validate

--- src/api/utils/barberShopValidation.ts
+++ src/api/utils/barberShopValidation.ts
@@ -4,13 +4,15 @@ import { prisma } from "../database";
 export async function validateBarberShopFields(
   name: string,
   address: string,
-  description: string
+  description: string,
+  rating: number,
+  opening_hours: string
 ) {
   const barberShopExist = await prisma.barberShop.findFirst({
     where: { name: name },
   });
 
-  if (!name || !address || !description) {
+  if (!name || !address || !description || !rating || !opening_hours) {
     return {
       error: true,
       message: "All fields is required",
